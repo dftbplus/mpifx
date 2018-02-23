80535f850e7db0e6b95902bc16f8ca0d8ff2f514
Bálint Aradi <aradi@uni-bremen.de>
2018-02-23 10:50:05 +0100

    Add wrapper for mpi_init_thread

--- src/mpifx_constants.F90
+++ src/mpifx_constants.F90
@@ -9,6 +9,7 @@ module mpifx_constants_module
   public :: MPI_MAX, MPI_MIN, MPI_SUM, MPI_PROD
   public :: MPI_LAND, MPI_BAND, MPI_LOR, MPI_BOR, MPI_LXOR ,MPI_BXOR
   public :: MPI_MAXLOC, MPI_MINLOC
+  public :: MPI_THREAD_SINGLE, MPI_THREAD_FUNNELED, MPI_THREAD_SERIALIZED, MPI_THREAD_MULTIPLE 
   
 end module mpifx_constants_module
 
